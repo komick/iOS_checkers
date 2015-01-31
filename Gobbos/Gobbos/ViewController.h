8a3183443568c13018a9328c603d1aefb1db98ca
jkomick <jkomick@ualberta.ca>
2015-01-31 16:48:32 -0700

    Project setup

---
+++ Gobbos/Gobbos/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gobbos
+//
+//  Created by Komick on 2015-01-31.
+//  Copyright (c) 2015 komick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

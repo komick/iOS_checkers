15eb4b95721de65fb645e51e8a841ff572028076
jkomick <jkomick@ualberta.ca>
2015-01-31 19:21:45 -0700

    Stubbing classes for game board controller and view.

---
+++ Gobbos/Gobbos/BoardView.h
@@ -0,0 +1,13 @@
+//
+//  BoardView.h
+//  Gobbos
+//
+//  Created by Komick on 2015-01-31.
+//  Copyright (c) 2015 komick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoardView : UIView
+
+@end
---
+++ Gobbos/Gobbos/BoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BoardViewController.h
+//  Gobbos
+//
+//  Created by Komick on 2015-01-31.
+//  Copyright (c) 2015 komick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoardViewController : UIViewController
+
+@end

8d554b4f3b16da73d614276e0d69e0dd64071d08
jkomick <jkomick@ualberta.ca>
2015-02-02 01:16:02 -0700

    Tokens now render smoothly as they are moved around the board.

--- Gobbos/Gobbos/TokenView.m
+++ Gobbos/Gobbos/TokenView.m
@@ -20,24 +20,37 @@
 
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /*UITouch *touch = [[event allTouches] anyObject];
-    CGPoint touchLocation = [touch locationInView:self->view];
-    if (CGRectContainsPoint(self.window.frame, touchLocation)) {
-        dragging = YES;
-        oldY = touchLocation.y;
-    }*/
+    //UITouch *touch = [[event allTouches] anyObject];
+    //CGPoint touchLocation = [touch locationInView:self->view];
+    //if (CGRectContainsPoint(self.window.frame, touchLocation)) {
+    //    dragging = YES;
+    //    oldY = touchLocation.y;
+    //}
     NSLog(@"TokenView - touchesBegan()");
 }
 
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    /*UITouch *touch = [[event allTouches] anyObject];
-    CGPoint touchLocation = [touch locationInView:self->view];
-    if (dragging) {
-        CGRect frame = self.window.frame;
-        frame.origin.y =  self.window.frame.origin.y + touchLocation.y - oldY;
-        self.window.frame = frame;
-    }*/
+    /*UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    if (touch.view == self) {
+        self.center =  touchLocation;
+        //CGRect newFrame = CGRectMake(touchLocation.x, touchLocation.y, 40.0f, 40.0f);
+        //self.frame = newFrame;
+    }
+    */
+    
+    UITouch *aTouch = [touches anyObject];
+    CGPoint loc = [aTouch locationInView:self];
+    CGPoint prevloc = [aTouch previousLocationInView:self];
+    
+    CGRect myFrame = self.frame;
+    float deltaX = loc.x - prevloc.x;
+    float deltaY = loc.y - prevloc.y;
+    myFrame.origin.x += deltaX;
+    myFrame.origin.y += deltaY;
+    [self setFrame:myFrame];
+    
     NSLog(@"TokenView - touchesMoved()");
 }
 
@@ -45,6 +58,13 @@
     //dragging = NO;
     NSLog(@"TokenView - touchesEnded()");
 }
+
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    NSLog(@"TokenView - touchesCancelled()");
+}
+
 
 
 @end

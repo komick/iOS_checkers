2d27fd7ccb6829e7bb008807a3669d26e16c1ec7
jkomick <jkomick@ualberta.ca>
2015-02-03 23:58:54 -0700

    Creating a BoardNodeView to store string name of owning BoardNode instance.

---
+++ Gobbos/Gobbos/BoardNodeView.h
@@ -0,0 +1,13 @@
+//
+//  BoardNodeView.h
+//  Gobbos
+//
+//  Created by Komick on 2015-02-03.
+//  Copyright (c) 2015 komick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoardNodeView : UIView
+
+@end
---
+++ Gobbos/Gobbos/BoardNodeView.m
@@ -0,0 +1,21 @@
+//
+//  BoardNodeView.m
+//  Gobbos
+//
+//  Created by Komick on 2015-02-03.
+//  Copyright (c) 2015 komick. All rights reserved.
+//
+
+#import "BoardNodeView.h"
+
+@implementation BoardNodeView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
